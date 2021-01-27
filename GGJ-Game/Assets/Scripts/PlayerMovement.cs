b183b890ef0189a29812574216c253d7afb62a54
barobon <46195201+barobon@users.noreply.github.com>
2021-01-27 21:27:19 +0900

    이동

--- GGJ-Game/Assets/Scripts/PlayerMovement.cs
+++ GGJ-Game/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rigid;
+    private Vector2 movement;
+
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.MovePosition(rigid.position + movement * speed * Time.fixedDeltaTime);
     }
 }
