74155ec7cd379773193e85583fa40838e5123ae8
KKhajiit <junesup0402@gmail.com>
2021-01-27 21:03:49 +0900

    Enemy, Floor, Wall, Door

---
+++ GGJ-Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    int health = 100;
+
+    public void GetDamage(int damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
