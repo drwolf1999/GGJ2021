2697f8a95cca4231961a711e9c4ae29409f8050b
barobon <46195201+barobon@users.noreply.github.com>
2021-01-31 01:32:38 +0900

    원거리 방향 오류 고쳐줘요,,,

--- GGJ-Game/Assets/Scripts/EnemyAI/EnemyRanged.cs
+++ GGJ-Game/Assets/Scripts/EnemyAI/EnemyRanged.cs
@@ -11,7 +11,8 @@ public class EnemyRanged : MonoBehaviour
     public bool isAwaken = false;
 
     public string bulletTag = "";
-    
+
+    public float rotateSpeed;
 
     private float timer;
     private float waitingTime;
@@ -41,7 +42,7 @@ public class EnemyRanged : MonoBehaviour
     {
         if(isAwaken)
         {
-            //LookAtPlayer();
+            LookAtPlayer();
             timer += Time.deltaTime;
             if (timer > waitingTime)
             {
@@ -82,9 +83,16 @@ public class EnemyRanged : MonoBehaviour
 
         //transform.LookAt(playerTransform.position);
 
-        Vector2 relativePos = playerTransform.position - transform.position;
-        Quaternion rotation = Quaternion.LookRotation(relativePos, Vector2.up);
+        //Vector2 relativePos = playerTransform.position - transform.position;
+        Vector2 direction = new Vector2(
+                transform.position.x - playerTransform.position.x,
+                transform.position.y - playerTransform.position.y
+            );
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion angleAxis = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
+        Quaternion rotation = Quaternion.Slerp(transform.rotation, angleAxis, rotateSpeed * Time.deltaTime);
         transform.rotation = rotation;
+        Debug.Log(transform.position);
     }
 
     public void AwakeEnemy()
